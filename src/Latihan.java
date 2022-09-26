14a7247193b7354ad5595bb925057a49f3b53378
ervanpnp <52595485+ervanpnp@users.noreply.github.com>
2022-09-26 11:17:43 +0700

    Create Latihan.java
    Update File Latihan

---
+++ src/Latihan.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FX506PE
+ */
+public class Latihan {
+    public static void main(String[] args) {
+        System.out.println("Halo");
+    }
+}
