b8aaedb5f8ed58242be19e3f0565bceb912c17c0
ervanpnp <52595485+ervanpnp@users.noreply.github.com>
2023-04-14 11:54:48 +0700

    Create Array.java

---
+++ src/modul4/Array.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul4;
+
+/**
+ *
+ * @author FX506PE
+ */
+public class Array {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+    }
+}
